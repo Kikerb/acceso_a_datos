4114f15d4ee024dea133e22433ef36268045b8a8
Kikerb <kikeriosbalderas@gmail.com>
2024-09-25 19:25:30 +0200

    Ej1

---
+++ 1/src/Ej1/Ejecicio1.java
@@ -0,0 +1,14 @@
+package Ej1;
+
+import java.io.File;
+
+public class Ejecicio1 {
+	
+	public static void main(String[] args) {
+    
+        
+        File directorio = new File("/Desktop/accesos_a_datos/1/cine_granada");
+        directorio.mkdir();
+	}
+
+}
