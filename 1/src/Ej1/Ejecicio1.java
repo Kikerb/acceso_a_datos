858cb9b08758f703754e66a4787c546f689ad93a
Kikerb <kikeriosbalderas@gmail.com>
2024-09-25 19:37:38 +0200

    Ej4

--- 1/src/Ej1/Ejecicio1.java
+++ 1/src/Ej1/Ejecicio1.java
@@ -7,8 +7,18 @@ public class Ejecicio1 {
 	public static void main(String[] args) {
     
         
-        File directorio = new File("/Desktop/accesos_a_datos/1/cine_granada");
-        directorio.mkdir();
-	}
+        File directorio = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/cine_granada");
+        
+        if (!directorio.exists()) {
+            boolean creado = directorio.mkdir();
+            if (creado) {
+                System.out.println("El directorio 'cine_granada' ha sido creado exitosamente.");
+            	} else {
+                System.out.println("No se pudo crear el directorio 'cine_granada'.");
+            }
+        	} else {
+            System.out.println("El directorio 'cine_granada' ya existe.");
+        }
+    }
 
 }
