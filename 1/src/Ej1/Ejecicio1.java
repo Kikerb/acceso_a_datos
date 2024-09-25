1428153bb6ce1ebe3c87ade15ae9d78f934925cb
Kikerb <kikeriosbalderas@gmail.com>
2024-09-25 20:17:21 +0200

    EJ4

--- 1/src/Ej1/Ejecicio1.java
+++ 1/src/Ej1/Ejecicio1.java
@@ -10,12 +10,7 @@ public class Ejecicio1 {
         File fichero = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/cine_granada");
         
         if (!fichero.exists()) {
-            boolean creado = fichero.mkdir();
-            if (creado) {
-                System.out.println("El directorio 'cine_granada' ha sido creado exitosamente.");
-                } else {
-                System.out.println("No se pudo crear el directorio 'cine_granada'.");
-            }
+        	fichero.mkdir();
             } else {
             System.out.println("El directorio 'cine_granada' ya existe.");
         }
@@ -25,12 +20,7 @@ public class Ejecicio1 {
        for (int i=0; i < 7;i++) {
             File ficheroDias = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/" + array[i]);
             if (!ficheroDias.exists()) {
-            	boolean creado = ficheroDias.mkdir();
-            	if(creado) {
-            		System.out.println("Los directorios con los dias de la semana han sido creado exitosamente.");	
-            		}else {
-                    System.out.println("No se podido crear Los directorios con los dias de la semana");
-                }
+            	ficheroDias.mkdir();
                 } else {
                 System.out.println("Los directorios con los dias de la semana ya existen.");
             }
