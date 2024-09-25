e2b4d44a0ed78b272ab63ee9b4c0e1e8747f7ce4
Kikerb <kikeriosbalderas@gmail.com>
2024-09-25 20:39:57 +0200

    Ej5
    Ejercicio 5

--- 1/src/Ej1/Ejecicio1.java
+++ 1/src/Ej1/Ejecicio1.java
@@ -10,36 +10,30 @@ public class Ejecicio1 {
         File fichero = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/cine_granada");
         
         if (!fichero.exists()) {
-            boolean creado = fichero.mkdir();
-            if (creado) {
-                System.out.println("El directorio 'cine_granada' ha sido creado exitosamente.");
-                } else {
-                System.out.println("No se pudo crear el directorio 'cine_granada'.");
-            }
+            fichero.mkdir();
+                System.out.println("El directorio 'cine_granada' se ha creado con ruta absoluta: " + fichero.getAbsolutePath());
             } else {
             System.out.println("El directorio 'cine_granada' ya existe.");
         }
-       
-       String[] array = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
         
+       
+       String[] array = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};  
        for (int i=0; i < 7;i++) {
             File ficheroDias = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/" + array[i]);
+            
             if (!ficheroDias.exists()) {
-            	for (int j =0; j <7;j++) {
-                    File ruta_antigua = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/" + array[j]);
-                    File ruta_nueva= new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/cine_granada/" + array[j]);
-                    ruta_antigua.renameTo(ruta_nueva);
-                }
-            	boolean creado = ficheroDias.mkdir();
-            	if(creado) {
-            		System.out.println(array[i] + " ha sido creado exitosamente.");	
-            		}else {
-                    System.out.println("No se podido crear " + array[i]);
-                }
+            	ficheroDias.mkdir();
+            		System.out.println(array[i] + " ha sido creado con ruta absoluta: " + ficheroDias.getAbsolutePath());	
                 } else {
                 System.out.println(array[i] + " ya existe.");
             }
             
+       }
+       
+       for (int j =0; j <7;j++) {
+           File ruta_antigua = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/" + array[j]);
+           File ruta_nueva= new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/cine_granada/" + array[j]);
+           ruta_antigua.renameTo(ruta_nueva);
        }
        
     }
