f0a3f9eb0bf584c08c385b145b7b171135ca1002
Kikerb <kikeriosbalderas@gmail.com>
2024-09-25 20:59:27 +0200

    Ej6
    ejercicio 6 definitivo

--- 1/src/Ej1/Ejecicio1.java
+++ 1/src/Ej1/Ejecicio1.java
@@ -1,45 +1,41 @@
 package Ej1;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Ejecicio1 {
     
-    public static void main(String[] args) {
-    
-        
+	public static void main(String[] args) throws IOException {
         File fichero = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/cine_granada");
-        
-        if (!fichero.exists()) {
-            fichero.mkdir();
-                System.out.println("El directorio 'cine_granada' se ha creado con ruta absoluta: " + fichero.getAbsolutePath());
-            } else {
-            System.out.println("El directorio 'cine_granada' ya existe.");
-        }
-        
-       
-       String[] array = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};  
-       for (int i=0; i < 7;i++) {
-            File ficheroDias = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/" + array[i]);
-            
-            if (!ficheroDias.exists()) {
-            	ficheroDias.mkdir();
-            		System.out.println(array[i] + " ha sido creado con ruta absoluta: " + ficheroDias.getAbsolutePath());	
-                } else {
-                System.out.println(array[i] + " ya existe.");
+            if(!fichero.exists()) {
+                fichero.mkdir();
+                System.out.println("Se ha creado el directorio con ruta absoluta: " + fichero.getAbsolutePath());
+                String[] array = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+                for (int i=0; i < 7;i++) {
+                    File ficheroDias = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/" + array[i]);
+                    if(!ficheroDias.exists()) {
+                        ficheroDias.mkdir();
+                        System.out.println(array[i] + " ha sido creado con ruta absoluta: " + ficheroDias.getAbsolutePath());
+                    
+                }else {
+                    System.out.println(array[i] + " ya existe");
+                }
+            }
+            System.out.println("");
+            System.out.println("----------ARCHIVOS CREADOS HASTA AHORA----------------");
+            System.out.println("");
+            for (int j =0; j <7;j++) {
+                File ruta_antigua = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/" + array[j]);
+                File ruta_nueva= new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/cine_granada/" + array[j]);
+                ruta_antigua.renameTo(ruta_nueva);
+                System.out.println(ruta_antigua.getAbsolutePath() + " se ha movido a " + ruta_nueva.getAbsolutePath());
+                System.out.println(ruta_nueva.getPath());
+                System.out.println("");
             }
             
-       }
-       System.out.println("---------------------Archivos creados hasta ahora---------------------");
-       System.out.println("");
-       for (int j =0; j <7;j++) {
-           File ruta_antigua = new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/" + array[j]);
-           File ruta_nueva= new File("C:/Users/AlumnoT/Desktop/acceso_a_datos/1/cine_granada/" + array[j]);
-           ruta_antigua.renameTo(ruta_nueva);
-           System.out.println(ruta_nueva.getPath());
-           System.out.println("");
-       }
-       
-       
+            }else {
+                System.out.println("El directorio 'cine_granada' ya existe");
+            }        
     }
 
 }
