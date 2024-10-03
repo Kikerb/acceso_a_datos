cfc28ceb0aeb9e841543f551e440a888a4d46784
Kikerb <kikeriosbalderas@gmail.com>
2024-10-03 18:59:47 +0200

    Ejercicio2 practica2

--- 2/src/Ej1/Ejercicio1.java
+++ 2/src/Ej1/Ejercicio1.java
@@ -7,16 +7,36 @@ import java.io.StreamTokenizer;
 public class Ejercicio1 {
 	
 	public static void main(String[] args) throws IOException {	  
-		StreamTokenizer lectortoken = new StreamTokenizer (new FileInputStream("C:/Users/AlumnoT/Desktop/acceso_a_datos/2/tema2_practica.txt"));
-			try {
-				while(lectortoken.nextToken()!= StreamTokenizer.TT_EOF) {
-					if(lectortoken.ttype == StreamTokenizer.TT_NUMBER ) {
-						System.out.print("Numero: ");
-						System.out.println((int)lectortoken.nval);
-					}
-				}
-			}catch (IOException error) {
-				error.printStackTrace();
-			}    
+		 StreamTokenizer lectortoken = new StreamTokenizer (new FileInputStream("C:/Users/AlumnoT/Desktop/acceso_a_datos/2/tema2_practica.txt"));
+	        boolean seguir = false;
+	                
+	        try {
+	            
+	            while(lectortoken.nextToken()!= StreamTokenizer.TT_EOF) {
+	                if(lectortoken.ttype =='>') {
+	                    seguir = true;
+	                }
+	                
+	                if (seguir == true) {
+	                    if(lectortoken.ttype == StreamTokenizer.TT_WORD ) {
+	                        String nombre = lectortoken.sval;
+	                        lectortoken.nextToken();
+	                        String apellido = lectortoken.sval;
+	                        System.out.println("Nombre: " + nombre + " " + apellido + " ");
+	                        seguir = false;
+	                    }
+	                }
+	                
+	                if(lectortoken.ttype == StreamTokenizer.TT_NUMBER) {
+	                    System.out.println("Numero: " + (int)lectortoken.nval);
+	                    System.out.println("");
+	                    
+	                }
+	            }
+	                    
+	                
+	        }catch (IOException error) {
+	            error.printStackTrace();
+	        }
 	}
 }
